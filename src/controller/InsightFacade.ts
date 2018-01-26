ee3f5221e42a87085e5b616f4089eb7500aedaf2
q7m0b <zelintian@gmail.com>
2018-01-25 17:16:34 -0800

    Colin 1

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,3 +1,4 @@
+import JSZip = require("jszip");
 import Log from "../Util";
 import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightResponse} from "./IInsightFacade";
 
@@ -11,7 +12,12 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<InsightResponse> {
-        return Promise.reject({code: -1, body: null});
+        const p = unzip(content);
+       // Log.trace("here");
+        if (p) {
+            return Promise.resolve({code: 204, body: null});
+        }
+        return Promise.reject({code: 404, body: null});
     }
 
     public removeDataset(id: string): Promise<InsightResponse> {
@@ -25,4 +31,48 @@ export default class InsightFacade implements IInsightFacade {
     public listDatasets(): Promise<InsightResponse> {
         return Promise.reject({code: -1, body: null});
     }
+}
+
+function unzip(content: string): Promise<boolean> {
+    const myZip = new JSZip();
+    let jsonFile: any;
+    const list: string[] = [] ;
+    return myZip.loadAsync(content, {base64: true})
+        .then(function (zip: JSZip) {
+            // Log.trace(zip.toString());
+            zip.forEach(function (path, file) {
+                file.async("text")
+                    .then(function (fileData) {
+                        // Log.trace(path);
+                       // Log.trace(fileData);
+                        try {
+                            jsonFile = JSON.parse(fileData);
+                            // Log.trace(jsonFile);
+                            // Log.trace(jsonFile.result.toString().split(",");
+                        } catch (err) {
+                            // Log.trace(path);
+                            throw err;
+                        }
+                        // jsonFile.async("text")
+                        //     .then(function () {
+                        //        // Log.trace(jsonFile.result["Title"]);
+                        //     });
+                        // if (jsonFile !== undefined && jsonFile.result === undefined) {
+                        //     Log.trace("valid JSON but no results");
+                        //     throw {message: "invalid dataset"};
+                        // }
+                        // if (jsonFile !== undefined) {
+                        //     list.push("courses_title:");
+                        //     list.push(jsonFile.Title);
+                        // }
+                    });
+                // Log.trace(jsonFile.result);
+                // Log.trace(path);
+            });
+            return true;
+        })
+        .catch(function (err: Error) {
+            Log.trace("invalid zip file format");
+            return false;
+        });
 }
