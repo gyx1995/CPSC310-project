1a4373cc8701b2ad62f650e38491c3d7c67f3371
q7m0b <zelintian@gmail.com>
2018-03-21 15:06:46 -0700

    Colin1

--- src/rest/Server.ts
+++ src/rest/Server.ts
@@ -6,6 +6,7 @@ import fs = require("fs");
 import restify = require("restify");
 import Log from "../Util";
 import {InsightResponse} from "../controller/IInsightFacade";
+import InsightFacade from "../controller/InsightFacade";
 
 /**
  * This configures the REST endpoints for the server.
@@ -14,6 +15,7 @@ export default class Server {
 
     private port: number;
     private rest: restify.Server;
+    private static facade: InsightFacade = new InsightFacade();
 
     constructor(port: number) {
         Log.info("Server::<init>( " + port + " )");
@@ -59,16 +61,18 @@ export default class Server {
                         res.header("Access-Control-Allow-Headers", "X-Requested-With");
                         return next();
                     });
-
                 // This is an example endpoint that you can invoke by accessing this URL in your browser:
                 // http://localhost:4321/echo/hello
                 that.rest.get("/echo/:msg", Server.echo);
-
+                that.rest.get("/datasets", Server.getDatasets);
+                that.rest.put("/dataset/:id/:kind", Server.addDataset);
+                that.rest.del("/dataset/:id", Server.removeDataset);
+                that.rest.use(restify.bodyParser());
+                that.rest.post("/query", Server.performQuery);
                 // NOTE: your endpoints should go here
 
                 // This must be the last endpoint!
                 that.rest.get("/.*", Server.getStatic);
-
                 that.rest.listen(that.port, function () {
                     Log.info("Server::start() - restify listening: " + that.rest.url);
                     fulfill(true);
@@ -87,7 +91,80 @@ export default class Server {
             }
         });
     }
+    private static getDatasets(req: restify.Request, res: restify.Response, next: restify.Next) {
+        Log.trace("Server::getDataSet(..) - params: ");
+        try {
+            // const result = Server.performEcho(req.params.msg);
+            const result = Server.facade.listDatasets();
+            result.then(function (r) {
+                res.json(r.code, r.body);
+            });
+        } catch (err) {
+            Log.error("Server::getDataset(..) - responding 400");
+            res.json(400, {error: err.message});
+        }
+        return next();
+    }
 
+    private static addDataset(req: restify.Request, res: restify.Response, next: restify.Next) {
+        const that = this;
+        Log.trace("Server::addDataset(..) - params: " + JSON.stringify(req.params));
+        try {
+            const id = req.params.id;
+            const kind = req.params.kind;
+            let buffer: any = [];
+            req.on("data", function onRequestData(chunk: any) {
+                buffer.push(chunk);
+            });
+            req.once("end", function () {
+                let concated = Buffer.concat(buffer);
+                req.body = concated.toString("base64");
+                Server.facade.addDataset(id, req.body, kind).then(function (result) {
+                    Log.info("Server::addDataset(..) - responding " + result.code);
+                    res.json(result.code, result.body);
+                }).catch(function (result) {
+                    res.json(result.code, result.body);
+                });
+            });
+        } catch (err) {
+            Log.error("Server::addDataset(..) - responding 400");
+            res.json(400, {error: err.message});
+        }
+        return next();
+    }
+
+    private static removeDataset(req: restify.Request, res: restify.Response, next: restify.Next) {
+        Log.trace("Server::remove(..) - params: " + JSON.stringify(req.params));
+        try {
+            const id = req.params.id;
+            Server.facade.removeDataset(id).then(function (result) {
+                Log.info("Server::remove(..) - responding " + result.code);
+                res.json(result.code, result.body);
+            }).catch(function (result) {
+                res.json(result.code, result.body);
+            });
+        } catch (err) {
+            Log.error("Server::remove(..) - responding 400");
+            res.json(400, {error: err.message});
+        }
+        return next();
+    }
+    private static performQuery(req: restify.Request, res: restify.Response, next: restify.Next) {
+        Log.trace("Server::Query(..) - params: " + JSON.stringify(req.body));
+        try {
+            Server.facade.performQuery(JSON.parse(req.body)).then(function (result) {
+                Log.info("Server::Query(..) - responding " + result.code);
+                res.json(result.code, result.body);
+                }).catch(function (result) {
+                    Log.trace("FAILLL");
+                    res.json(result.code, result.body);
+            });
+        } catch (err) {
+            Log.error("Server::Query(..) - responding 400");
+            res.json(400, {error: err.message});
+        }
+        return next();
+    }
     // The next two methods handle the echo service.
     // These are almost certainly not the best place to put these, but are here for your reference.
     // By updating the Server.echo function pointer above, these methods can be easily moved.
--- test/Server.spec.ts
+++ test/Server.spec.ts
@@ -4,6 +4,9 @@ import InsightFacade from "../src/controller/InsightFacade";
 import chai = require("chai");
 
 import chaiHttp = require("chai-http");
+import { expect } from "chai";
+import Log from "../src/Util";
+import TestUtil from "./TestUtil";
 
 describe("Facade D3", function () {
 
@@ -15,11 +18,13 @@ describe("Facade D3", function () {
     before(function () {
         facade = new InsightFacade();
         server = new Server(4321);
+        server.start();
         // TODO: start server here once and handle errors properly
     });
 
     after(function () {
         // TODO: stop server here once!
+        server.stop();
     });
 
     beforeEach(function () {
@@ -27,31 +32,96 @@ describe("Facade D3", function () {
     });
 
     afterEach(function () {
+        // server.stop();
         // might want to add some process logging here to keep track of what"s going on
     });
 
     // TODO: read your courses and rooms datasets here once!
-
-    // Hint on how to test PUT requests
-    /*
-    it("PUT test for courses dataset", function () {
+    it("Get list of datasets", function () {
         try {
-            return chai.request(URL)
-                .put(YOUR_PUT_URL)
-                .attach("body", YOUR_COURSES_DATASET, COURSES_ZIP_FILENAME)
-                .then(function (res: Response) {
+            return chai.request("http://localhost:4321")
+                .get("/datasets")
+                .then(function (res: any) {
                     // some logging here please!
-                    expect(res.status).to.be.equal(204);
+                    Log.trace(res.code);
+                    expect(res.status).to.be.equal(200);
                 })
-                .catch(function (err) {
+                .catch(function (err: Error) {
                     // some logging here please!
                     expect.fail();
                 });
         } catch (err) {
             // and some more logging here!
+            Log.trace(err.toString());
+            expect.fail();
         }
     });
-    */
-
+    // Hint on how to test PUT requests
+    it("PUT test for courses dataset", function () {
+        try {
+            return chai.request("http://localhost:4321")
+                .put("/dataset/courses/courses")
+                .attach("body", "./test/data/courses.zip", "courses.zip")
+                .then(function (res: any) {
+                    expect(res.status).to.be.equal(204);
+                })
+                .catch(function (err: Error) {
+                    expect.fail();
+                });
+        } catch (err) {
+            // and some more logging here!
+            Log.trace(err.toString());
+            expect.fail();
+        }
+    });
+    it("POST a simple query", function () {
+        try {
+            return chai.request("http://localhost:4321")
+                .post("/query")
+                // .set("content-type", "application/x-www-form-urlencoded")
+                .send("{\n" +
+                    "\t\"WHERE\": {\n" +
+                    "        \"GT\": { \"courses_avg\": 70 }\n" +
+                    "\t},\n" +
+                    "\t\"OPTIONS\": {\n" +
+                    "\t\t\"COLUMNS\": [\"courses_title\", \"overallAvg\"]\n" +
+                    "\t},\n" +
+                    "\t\"TRANSFORMATIONS\": {\n" +
+                    "\t\t\"GROUP\": [\"courses_title\"],\n" +
+                    "\t\t\"APPLY\": [{\n" +
+                    "\t\t\t\"overallAvg\": {\n" +
+                    "\t\t\t\t\"AVG\": \"courses_avg\"\n" +
+                    "\t\t\t}\n" +
+                    "\t\t}]\n" +
+                    "\t}\n" +
+                    "}")
+                .then(function (res: any) {
+                    expect(res.status).to.be.equal(200);
+                })
+                .catch(function (err: Error) {
+                    expect.fail();
+                });
+        } catch (err) {
+            // and some more logging here!
+            Log.trace(err.toString());
+            expect.fail();
+        }
+    });
+    it("Delete test for courses dataset", function () {
+        try {
+            return chai.request("http://localhost:4321")
+                .del("/dataset/courses")
+                .then(function (res: any) {
+                    expect(res.status).to.be.equal(204);
+                })
+                .catch(function (err: Error) {
+                    expect.fail();
+                });
+        } catch (err) {
+            // and some more logging here!
+            Log.trace(err.toString());
+            expect.fail();
+        }
+    });
     // The other endpoints work similarly. You should be able to find all instructions at the chai-http documentation
 });
