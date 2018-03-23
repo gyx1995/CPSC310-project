86ae56060792fca678d51cd159a10cf1c0fc9359
Guanyu Xing <guanx95@hotmail.com>
2018-03-23 15:59:36 -0700

    REX5

--- src/controller/DoQuery.ts
+++ src/controller/DoQuery.ts
@@ -52,8 +52,10 @@ export default class DoQuery {
     public query(query: IQueryRequest): Promise<any> {
         // Log.trace("Length:" + this.array.length.toString());
         const that: any = this;
-        const id = that.findID(query["WHERE"]);
-        Log.trace("!!!!!!!!!!!id " + id );
+        let id = that.findID(query["WHERE"]);
+        if (id === "") {
+            id = that.findID_emptyWhere(query);
+        }
         that.array = that.insight.getDataset(id);
         const re: any = [];
         return new Promise(function (fulfill, reject) {
@@ -62,7 +64,7 @@ export default class DoQuery {
             //     Log.trace(d["courses_avg"]);
             // }
             //////////////////////////////
-            const result: any = that.filter(where);
+            const   result = that.filter(where);
             const array = that.array;
             let c = 0;
             for (let i = 0; i < array.length; i++) {
@@ -80,7 +82,6 @@ export default class DoQuery {
             //         re.push(arr[i]);
             //     }
             // }
-            Log.trace("after filter and select start");
             const options: any = query["OPTIONS"];
             let finalresult: any;
             if ("TRANSFORMATIONS" in query) {
@@ -89,7 +90,6 @@ export default class DoQuery {
             } else {
                 finalresult = that.select(re, options);
             }
-            Log.trace("after select");
             if (c === 0) {
                 finalresult = [];
             }
@@ -113,7 +113,7 @@ export default class DoQuery {
         });
     }
     public doTrans(rawResult: any, trans: any, option: any ): any {
-        Log.trace("HIHIHIHIH");
+        // Log.trace("HIHIHIHIH");
         const that: any = this;
         let transResult: any = [];
         const finalResult: any = [];
@@ -186,7 +186,7 @@ export default class DoQuery {
                     // const aa = new Decimal(1.3);
                     // aa.toNumber();
                     const num = new Decimal(acc[groups.length - 1][appKey]).toNumber();
-                    Log.trace(num.toString());
+                    // Log.trace(num.toString());
                     groups[groups.length - 1][appKey] = Number((num / new Decimal(count).toNumber())
                         .toFixed(2));
                 } else if ("SUM" in val) {
@@ -211,7 +211,7 @@ export default class DoQuery {
             const entry = transResult[i];
             if (i > 0 && !that.compare(transResult[i - 1], entry, group)) {
                 // not in the same group
-                Log.trace("NO!!! NOT SAME GROUP");
+                // Log.trace("NO!!! NOT SAME GROUP");
                 last_group();
                 // groups[groupsCount]
                 create_new_group(entry);
@@ -367,28 +367,27 @@ export default class DoQuery {
     // }
     public isValid(query: IQueryRequest): number {
         if (typeof query === "undefined" || query == null || Object.keys(query).length < 0) {
-            Log.trace("1");
             return 400;
         }
         if (!("WHERE" in query) || !("OPTIONS" in query)) {
-            Log.trace("2");
             return 400;
         }
         if ("TRANSFORMATIONS" in query) {
             const trans = query["TRANSFORMATIONS"];
             if (!("APPLY" in trans) || !("GROUP" in trans)) {
-                Log.trace(" transformation missing something");
                 return 400;
             }
         }
         const where: any  = query["WHERE"];
         const dataID = this.findID(where);
-        if (dataID !== "courses" && dataID !== "rooms") {
-            return 400;
-        }
-        const w = this.condValid(where);
-        if ((w !== 200)) {
-            return w;
+        if (dataID !== "") {
+            if (dataID !== "courses" && dataID !== "rooms") {
+                return 400;
+            }
+            const w = this.condValid(where);
+            if ((w !== 200)) {
+             return w;
+            }
         }
         if ("OPTIONS" in query) {
             const options: any  = query["OPTIONS"];
@@ -400,7 +399,6 @@ export default class DoQuery {
         if (i !== 200) {
             return i;
         }
-        Log.trace("the query is valid");
         return 200;
     }
     public findID(cond: any): any {
@@ -437,8 +435,27 @@ export default class DoQuery {
         }
         return dataID;
     }
+    public findID_emptyWhere(query: any ): any {
+        let dataID = "";
+        let ele = "";
+        if ("TRANSFORMATIONS" in query) {
+            const trans = query["TRANSFORMATIONS"];
+            const group = trans["GROUP"];
+            ele = group[0];
+        } else {
+            const options: any = query["OPTIONS"];
+            const col = options["COLUMNS"];
+            ele = col[0];
+        }
+        const index = ele.indexOf("_");
+        dataID = ele.substring(0 , index);
+        return dataID;
+    }
     public isKeyValid(query: IQueryRequest): number {
-        const dataID = this.findID(query["WHERE"]);
+        let dataID = this.findID(query["WHERE"]);
+        if (dataID === "") {
+            dataID = this.findID_emptyWhere(query);
+        }
         const options: any = query["OPTIONS"];
         const columns = options["COLUMNS"];
         const applyKeys = [];
@@ -451,11 +468,9 @@ export default class DoQuery {
                     if (applyKeys.indexOf(Object.keys(a)[0]) === -1) {
                         applyKeys.push(Object.keys(a)[0]);
                     } else {
-                        Log.trace("the key in APPLY is not unique");
                         return 400;
                     }
                 } else {
-                    Log.trace("Invalid Apply Keys");
                     return 400;
                 }
             }
@@ -466,7 +481,6 @@ export default class DoQuery {
                     const v2 = Object.values(v)[0];
                     if ( "MAX" in v || "MIN" in v || "SUM" in v ||  "AVG" in v) {
                         if (this.courseNkey.indexOf(v2) === -1) {
-                            Log.trace("in courses : Max||MIN||SUM||AVG with non Number");
                             return 400;
                         }
                     } else if ( "COUNT" in v) {
@@ -479,7 +493,6 @@ export default class DoQuery {
                 }
                 for (const c of columns) {
                     if (group.indexOf(c) === -1 && applyKeys.indexOf(c) === -1) {
-                        Log.trace("trans courses fals1");
                         return 400;
                     }
                 }
@@ -489,7 +502,6 @@ export default class DoQuery {
                     const v2 = Object.values(v)[0];
                     if ( "MAX" in v || "MIN" in v || "SUM" in v ||  "AVG" in v) {
                         if (this.roomNkey.indexOf(v2) === -1) {
-                            Log.trace("in rooms : Max||MIN||SUM||AVG with non Number");
                             return 400;
                         }
                     } else if ( "COUNT" in v) {
@@ -502,7 +514,6 @@ export default class DoQuery {
                 }
                 for (const c of columns) {
                     if (group.indexOf(c) === -1 && applyKeys.indexOf(c) === -1) {
-                        Log.trace("trans rooms fals1");
                         return 400;
                     }
                 }
@@ -511,14 +522,12 @@ export default class DoQuery {
             if (dataID === "courses") {
                 for (const c of columns) {
                     if (this.coursekey.indexOf(c) === -1) {
-                        Log.trace("courses fals1");
                         return 400;
                     }
                 }
             } else {
                 for (const c of columns) {
                     if (this.roomkey.indexOf(c) === -1) {
-                        Log.trace("rooms fals1");
                         return 400;
                     }
                 }
@@ -528,14 +537,10 @@ export default class DoQuery {
             const order = options["ORDER"];
             if ((columns.indexOf(order) === -1)) {
                 if (!("keys" in order) || !("dir" in order)) {
-                    Log.trace("Option order key is wrong");
                     return 400;
                 } else {
                     for (const k of order["keys"]) {
                         if (columns.indexOf(k) === -1 ) {
-                            Log.trace("Order key: " + k);
-                            Log.trace("Columns: " + columns);
-                            Log.trace("Order key needs to be included in columns (400)");
                             return 400;
                         }
                     }
@@ -551,28 +556,22 @@ export default class DoQuery {
     public condValid(cond: any ): number {
         const dataID = this.findID(cond);
         if (Object.keys(cond).length === 0 ) {
-            Log.trace("1");
             return 400;
         } else {
             if ("NOT" in cond) {
                 const not = cond["NOT"];
                 if (not.length < 1 ) {
-                    Log.trace("notNOCondFalse");
                     return 400;
                 } else {
-                    Log.trace("check not");
                     if ( 400 === this.condValid(not)) {
                         return 400;
                     }
-                    Log.trace("check not finished");
                 }
             } else if ("AND" in cond) {
                 const and = cond["AND"];
                 if (and.length < 1 ) {
-                    Log.trace("andNOCondFalse");
                     return 400;
                 } else {
-                    Log.trace("check and");
                     for (const suband of and ) {
                         if ( 400 === this.condValid(suband)) {
                             return 400;
@@ -582,10 +581,8 @@ export default class DoQuery {
             } else if ("OR" in cond) {
                 const or = cond["OR"];
                 if (or.length < 1  ) {
-                    Log.trace("orNOCondFalse");
                     return 400;
                 } else {
-                    Log.trace("check OR");
                     for (const subor of or) {
                         if ( 400 === this.condValid(subor)) {
                             return 400;
@@ -596,86 +593,71 @@ export default class DoQuery {
                 const a = cond["EQ"];
                 const num = Object.keys(a).length;
                 if (num !== 1 ) {
-                    Log.trace("EQNOCondFalse");
                     return 400;
                 }
                 if (dataID === "courses") {
                     if (this.courseNkey.indexOf(Object.keys(a)[0]) === -1) {
-                        Log.trace("EQWrongKeyCondFalse");
                         return 400;
                     }
                 } else {
                     if (this.roomNkey.indexOf(Object.keys(a)[0]) === -1) {
-                        Log.trace("EQWrongKeyCondFalse");
                         return 400;
                     }
                 }
                 const v = Object.values(a)[0];
                 if (!(isNumber(v))) {
-                    Log.trace("the value of EQ is not a number");
                     return 400;
                 }
             } else if ("LT" in cond) {
                 const a = cond["LT"];
                 const num = Object.keys(a).length;
                 if (num !== 1 ) {
-                    Log.trace("LTNOCondFalse");
                     return 400;
                 }
                 if (dataID === "courses") {
                     if (this.courseNkey.indexOf(Object.keys(a)[0]) === -1) {
-                        Log.trace("LTWrongKeyCondFalse");
                         return 400;
                     }
                 } else {
                     if (this.roomNkey.indexOf(Object.keys(a)[0]) === -1) {
-                        Log.trace("LTWrongKeyCondFalse");
                         return 400;
                     }
                 }
                 const v = Object.values(a)[0];
                 if (!(isNumber(v))) {
-                    Log.trace("the value of LT is not a number");
                     return 400;
                 }
             } else if ("GT" in cond) {
                 const a = cond["GT"];
                 const num = Object.keys(a).length;
                 if (num !== 1 ) {
-                    Log.trace("GTNOCondFalse");
                     return 400;
                 }
                 if (dataID === "courses") {
                     if (this.courseNkey.indexOf(Object.keys(a)[0]) === -1) {
-                        Log.trace("GTWrongKeyCondFalse");
                         return 400;
                     }
                 } else {
                     if (this.roomNkey.indexOf(Object.keys(a)[0]) === -1) {
-                        Log.trace("GTWrongKeyCondFalse");
                         return 400;
                     }
                 }
                 const v = Object.values(a)[0];
                 if (!(isNumber(v))) {
-                    Log.trace("the value of GT is not a number");
                     return 400;
                 }
             } else if ("IS" in cond) {
                 const a = cond["IS"];
                 const num = Object.keys(a).length;
                 if (num !== 1 ) {
-                    Log.trace("ISNOCondFalse");
                     return 400;
                 }
                 if (dataID === "courses") {
                     if (this.courseSkey.indexOf(Object.keys(a)[0]) === -1) {
-                        Log.trace("ISWrongKeyCondFalse");
                         return 400;
                     }
                 } else {
                     if (this.roomSkey.indexOf(Object.keys(a)[0]) === -1) {
-                        Log.trace("GTWrongKeyCondFalse");
                         return 400;
                     }
                 }
@@ -687,27 +669,31 @@ export default class DoQuery {
                     return 200;
                 }
                 if (!(isString(v)) || !(/^[*]?[^*]+[*]?$/).test(v)) { // regular expression
-                    Log.trace("the value of IS is not a string");
                     return 400;
                 }
             } else {
-                Log.trace("invalid LOG in WHERE");
                 return 400;
             }
             return 200;
         }
     }
     public filter(where: any): any {
+        const dataID = this.findID(where);
         const arr: any = this.array;
         const ans: any = [];
-        for (let i = 0; i < arr.length; i++) {
-            ans[i] = 0;
+        if (dataID === "") {
+            for (let i = 0; i < arr.length; i++) {
+                ans[i] = 1;
+            }
+        } else {
+            for (let i = 0; i < arr.length; i++) {
+                ans[i] = 0;
+            }
         }
         // Log.trace(arr.length);
         /////////////////////////////
         ////// this.insightFacade = new InsightFacade();
         ///////////////////////////
-        Log.trace("111");
         if ("GT" in where) {
             const key = Object.keys(where["GT"])[0];
             const v = where["GT"][key];
@@ -768,15 +754,12 @@ export default class DoQuery {
             }
         }
         if ("IS" in where) {
-            Log.trace("in IS");
             const key = Object.keys(where["IS"])[0];
             let v = where["IS"][key];
             let subis = "";
             ///////////////////////////////////////////
             if (v.indexOf("*") !== -1 ) {
-                Log.trace("yes, there is a *!!!!!!!!!!!!!!!");
                 const idx = v.indexOf("*");
-                Log.trace(idx);
                 switch (idx) {
                     case 0:
                         if (v.length === 1 ) {
@@ -817,9 +800,7 @@ export default class DoQuery {
                         }
                         break;
                     case v.length - 1:
-                        Log.trace("it is in the last one");
                         subis = v.substring(0, v.length - 1);
-                        Log.trace("it isn in the last place: " + subis);
                         const ln = subis.length;
                         for (let i = 0; i < arr.length; i++) {
                             // Log.trace(d[key]);
@@ -834,12 +815,10 @@ export default class DoQuery {
                         }
                         break;
                     default:
-                        Log.trace("it is in the  middle --- error");
                         break;
                 }
             } else {
                 //////////////////////////////////////////
-                Log.trace(v);
                 for (let i = 0; i < arr.length; i++) {
                     // Log.trace(d[key]);
                     if (arr[i][key] === v) {
@@ -855,7 +834,6 @@ export default class DoQuery {
             const or = where["OR"];
             // Log.trace(or.length + " length");
             const ff: any = this.filter(or[0]);
-            Log.trace(ff.length + " length");
             for (const o of or) {
                 const rf: any = this.filter(o);
                 // Log.trace(rf.length + " rf length");
@@ -870,11 +848,8 @@ export default class DoQuery {
             }
         }
         if ("AND" in where) {
-            Log.trace("in and");
             const and = where["AND"];
-            Log.trace(and.length + "and  length");
             const ff = this.filter(and[0]);
-            Log.trace(ff.length + " ff length");
             for (const o of and) {
                 const rf: any = this.filter(o);
                 // Log.trace(rf.length + " rf length");
@@ -906,7 +881,6 @@ export default class DoQuery {
             // }
         }
         if ("NOT" in where) {
-            Log.trace("in NOT");
             const not = where["NOT"];
             const rf = this.filter(not);
             for (let i = 0; i < rf.length; i++) {
@@ -923,7 +897,6 @@ export default class DoQuery {
         // for (const d of re) {
         //     Log.trace(d[key]);
         // }
-        Log.trace("inside filter done");
         // Log.trace(ans);
         return ans;
     }
