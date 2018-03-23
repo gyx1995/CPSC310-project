62000ace3a2e505496c4480e04ef4fced54f303a
Guanyu Xing <guanx95@hotmail.com>
2018-03-23 14:56:07 -0700

    REX4

--- frontend/public/query-sender.js
+++ frontend/public/query-sender.js
@@ -8,16 +8,18 @@ CampusExplorer.sendQuery = function(query) {
     return new Promise(function(fulfill, reject) {
         //console.log("CampusExplorer.sendQuery not implemented yet.");
         let httpRequest = new XMLHttpRequest();
+        httpRequest.open('POST','http://localhost:4321/query');
         httpRequest.onload = function() {
-            if (this.readyState === 4) {
-                if (this.status === 200){
+            if (this.status === 200){
                     fulfill(this.responseText);
                 } else {
                     reject(this.responseText);
-                }
             }
         };
-        httpRequest.open('POST','http://localhost:4321/query');
-        httpRequest.send(JSON.stringify(query));
+        try {
+            httpRequest.send(JSON.stringify(query));
+        }catch (err){
+            reject({error:err});
+        }
     });
 };
